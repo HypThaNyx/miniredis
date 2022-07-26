2bb48e005db2ce1e70e454fdd39dfcc7d96e7ebb
HypThaNyx <wesleyyds@hotmail.com>
2022-07-26 00:03:21 -0300

    Spring Initializr

---
+++ src/main/java/com/aquiris/miniredis/MiniredisApplication.java
@@ -0,0 +1,13 @@
+package com.aquiris.miniredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniredisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aquiris/miniredis/MiniredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquiris.miniredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
