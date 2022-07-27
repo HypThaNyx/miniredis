9b1301773e80f74a4344c798a435e12007207099
HypThaNyx <wesleyyds@hotmail.com>
2022-07-27 00:36:44 -0300

    feat(service): start service with repositories constructor DI

---
+++ src/main/java/com/aquiris/miniredis/service/MiniRedisService.java
@@ -0,0 +1,20 @@
+package com.aquiris.miniredis.service;
+
+import com.aquiris.miniredis.entity.NElement;
+import com.aquiris.miniredis.repository.NElementRepository;
+import com.aquiris.miniredis.repository.SortedSetRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MiniRedisService {
+    private final NElementRepository nElementRepository;
+    private final SortedSetRepository sortedSetRepository;
+
+    MiniRedisService(NElementRepository nElementRepository, SortedSetRepository sortedSetRepository) {
+        this.nElementRepository = nElementRepository;
+        this.sortedSetRepository = sortedSetRepository;
+    }
+
+}
