85de2d7ba631e9c113740f4b3412e165a04328eb
HypThaNyx <wesleyyds@hotmail.com>
2022-07-28 15:11:52 -0300

    fix(entity): set children's parent

--- src/main/java/com/aquiris/miniredis/entity/SortedSet.java
+++ src/main/java/com/aquiris/miniredis/entity/SortedSet.java
@@ -4,6 +4,7 @@ package com.aquiris.miniredis.entity;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
@@ -16,15 +17,20 @@ public class SortedSet extends Ledger {
         this.elementos = elementos;
     }
     @OneToMany(mappedBy = "sorted_set")
-    private List<ZElement> elementos;
+    private List<ZElement> elementos = new ArrayList<ZElement>();
 
     public List<ZElement> getElementos() {
         return elementos;
     }
 
     public void setElementos(List<ZElement> elementos) {
+        elementos.forEach(elemento -> elemento.setSorted_set(this));
         this.elementos = elementos;
     }
 
+    public void addToElementos(ZElement elemento) {
+        elemento.setSorted_set(this);
+        this.elementos.add(elemento);
+    }
 
 }
